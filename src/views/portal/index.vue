3fda1ea6eb737fdb99acf4d62225dd533cfc9974
zjy7 <zjy7@hotmail.com>
2021-03-08 16:32:15 +0800

    links

--- src/views/portal/index.vue
+++ src/views/portal/index.vue
@@ -10,7 +10,14 @@ export default {
   methods:{
     clickProvider(){
       window.open('https://leaflet-extras.github.io/leaflet-providers/preview/','_blank')
+    },
+    clickPhoto(){
+      window.open('https://blog.mastermaps.com/2014/08/showing-geotagged-photos-on-leaflet-map.html','_blank')
+    },
+    clickPluseIcon(){
+      window.open('https://github.com/mapshakers/leaflet-icon-pulse','_blank')
     }
+    
   }
 }
 </script>
